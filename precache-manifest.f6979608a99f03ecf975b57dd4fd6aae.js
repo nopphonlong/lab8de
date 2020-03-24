ba167bfe484b1c6096d5a5b04075911eda0a901d
nopphonlong <58941386+nopphonlong@users.noreply.github.com>
2020-03-24 21:29:10 +0700

    Updates

---
+++ precache-manifest.f6979608a99f03ecf975b57dd4fd6aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e168aa34a8c1949143bf0171055784e8",
+    "url": "/lab8de/index.html"
+  },
+  {
+    "revision": "22cc0a7673d17fadef49",
+    "url": "/lab8de/static/css/main.04418a4e.chunk.css"
+  },
+  {
+    "revision": "ab1cec3c7ee16ded5f60",
+    "url": "/lab8de/static/js/2.3165bfb6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/lab8de/static/js/2.3165bfb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22cc0a7673d17fadef49",
+    "url": "/lab8de/static/js/main.24ec4fda.chunk.js"
+  },
+  {
+    "revision": "232222afb1b7d81ac819",
+    "url": "/lab8de/static/js/runtime-main.2bf8b49c.js"
+  }
+]);
